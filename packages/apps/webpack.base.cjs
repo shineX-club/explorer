6a2d69f7dff8807dacff5a03ee2a4b1000d5cef2
Founy_ <zzboyprince@gmail.com>
2022-03-15 20:27:20 +0800

    update: dist to public

--- packages/apps/webpack.base.cjs
+++ packages/apps/webpack.base.cjs
@@ -138,7 +138,7 @@ function createWebpack (context, mode = 'production') {
       chunkFilename: '[name].[chunkhash:8].js',
       filename: '[name].[contenthash:8].js',
       globalObject: '(typeof self !== \'undefined\' ? self : this)',
-      path: path.join(context, '../../dist'),
+      path: path.join(context, '../../public'),
       publicPath: ''
     },
     performance: {
